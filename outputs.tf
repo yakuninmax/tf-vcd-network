9f6bda7369925655816f9bf257e020cec9ed74f0
Max Yakunin <58250827+yakuninmax@users.noreply.github.com>
2021-02-09 12:21:51 +0300

    Add and fix outputs
    Add and fix outputs

--- outputs.tf
+++ outputs.tf
@@ -1,7 +1,11 @@
-output "network" {
-  value = length(var.networks) != 0 ? vcd_network_routed.network[*] : null
+output "networks" {
+  value = length(var.networks) != 0 ? vcd_network_routed.network[*].name : null
+}
+
+output "subnets" {
+  value = length(var.networks) != 0 ? var.networks[*].cidr : null
 }
 
 output "vapp" {
-  value = var.vapp_name != "" ? vcd_vapp.vapp : null
+  value = var.vapp_name != "" ? vcd_vapp.vapp.vapp_name : null
 }
