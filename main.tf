fc86d8cd26d2a7409ac9e3c15616967992405f3e
Max Yakunin <yakunin.max@gmail.com>
2021-03-01 13:00:17 +0300

    Change input variables

--- main.tf
+++ main.tf
@@ -1,15 +1,15 @@
 # Create new org network
 resource "vcd_network_routed" "network" {
-  name         = var.network.name
+  name         = var.name
   edge_gateway = data.vcd_edgegateway.edge.name
-  gateway      = cidrhost(var.network.cidr, 1)
-  netmask      = cidrnetmask(var.network.cidr)
-  dns1         = var.network.dns1 != "" ? var.network.dns1 : null
-  dns2         = var.network.dns2 != "" ? var.network.dns2 : null
+  gateway      = cidrhost(var.cidr, 1)
+  netmask      = cidrnetmask(var.cidr)
+  dns1         = var.dns1 != "" ? var.dns1 : null
+  dns2         = var.dns2 != "" ? var.dns2 : null
         
   static_ip_pool {
-    start_address = cidrhost(var.network.cidr, 10)
-    end_address   = cidrhost(var.network.cidr, 99)
+    start_address = cidrhost(var.cidr, 10)
+    end_address   = cidrhost(var.cidr, 99)
   }
 }
 
@@ -20,22 +20,22 @@ resource "vcd_nsxv_snat" "default-snat-rule" {
   edge_gateway       = data.vcd_edgegateway.edge.name
   network_type       = "ext"
   network_name       = tolist(data.vcd_edgegateway.edge.external_network)[0].name
-  original_address   = var.network.cidr
+  original_address   = var.cidr
   translated_address = data.vcd_edgegateway.edge.external_network_ips[0]
-  description        = "From ${var.network.name} to Internet"
+  description        = "From ${var.name} to Internet"
 }
 
 # Create default firewall rules
 resource "vcd_nsxv_firewall_rule" "default-firewall-rule" {  
   edge_gateway = data.vcd_edgegateway.edge.name
-  name         = "From ${var.network.name} to Internet"
+  name         = "From ${var.name} to Internet"
 
   source {
-    ip_addresses = [var.network.cidr]
+    ip_addresses = [var.cidr]
   }
 
   destination {
-    gateway_interfaces = ["external", var.network.name]
+    gateway_interfaces = ["external", var.name]
   }
 
   service {
--- variables.tf
+++ variables.tf
@@ -1,10 +1,19 @@
-variable "network" {
-  type = object({
-    name = string
-    cidr = string
-    dns1 = string
-    dns2 = string
-  })
-  
-  description = "Network parameters"
+variable "name" {
+  type        = string
+  description = "Network name"
+}
+
+variable "cidr" {
+  type        = string
+  description = "Network CIDR"
+}
+
+variable "dns1" {
+  type        = string
+  description = "Network first DNS server address"
+}
+
+variable "dns2" {
+  type        = string
+  description = "Network second DNS server address"
 }
